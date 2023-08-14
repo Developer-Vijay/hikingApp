2bc8f2d10dd434537e26f7564352d2b2c0a9ef69
Developer-Vijay <vijaysahoo147@gmail.com>
2023-08-15 00:54:17 +0530

    Initial Commit

---
+++ hiking App/hiking_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  hiking_AppApp.swift
+//  hiking App
+//
+//  Created by Vijay Sahoo on 15/08/23.
+//
+
+import SwiftUI
+
+@main
+struct hiking_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
